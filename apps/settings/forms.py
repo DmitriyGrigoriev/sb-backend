005dab17b4e3a54ea1421807c666bfbb2759a714
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-19 15:04:42 +0300

    Add form for the service price model

--- apps/settings/forms.py
+++ apps/settings/forms.py
@@ -1,18 +1,28 @@
 from django import forms
-from django.forms import ModelForm, ValidationError
-
+from django.forms import ModelForm
+from django.forms.models import BaseInlineFormSet
+from django.utils.translation import ugettext_lazy as _
 from .models import Service
 
 class ServiceModelAdminForm(ModelForm):
+    code = forms.CharField(required=True,
+                           label=_('Код Услуги'),
+                           max_length=20,
+                           empty_value=''
+                           )
     class Meta:
         model = Service
         fields = '__all__'
 
-    # def clean_blocked(self):
-    #     result = True
-    #     cleaned_data = super().clean()
-    #     blocked = cleaned_data.get('blocked')
-    #     if blocked:
-    #         result = False
-    #         raise forms.ValidationError('invalid!')
-    #     return result
+
+class ServicePriceModelAdminForm(BaseInlineFormSet):
+    price = forms.DecimalField(required=True)
+
+    def clean(self):
+        """Check that at least one service has been entered."""
+        super().clean()
+        if any(self.errors):
+            return
+        if not any(cleaned_data and not cleaned_data.get('DELETE', False)
+            for cleaned_data in self.cleaned_data):
+            raise forms.ValidationError(_('Необходимо добавить хотя бы одну цену для «Услуги»'))
