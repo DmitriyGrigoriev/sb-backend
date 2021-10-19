e2252f24f62287935285759e681910b4e4a7f2f4
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-19 16:01:15 +0300

    Add ServicePriceManager
    feat: Added a fresh_price method.

--- apps/settings/managers.py
+++ apps/settings/managers.py
@@ -1,29 +1,11 @@
-from typing import Optional
-# from django.core.exceptions import ObjectDoesNotExist
-from .models import *
 from .querysets import *
-# from common.managers import CustomManager
 
-class BillingSetupManager(models.Manager):
-    def get_queryset(self):
-        return BillingSetupQuerySet(self.model, using=self._db)
-
-    def get_series_no_setup(self):
-        return self.get_queryset().get_series_no_setup()
-
-
-class NoSeriesManager(models.Manager):
-    def get_queryset(self):
-        return NoSeriesQuerySet(self.model, using=self._db)
-
-    def get_series_no_setup(self):
-        return self.get_queryset().get_series_no_setup()
 
 class NoSeriesLineManager(models.Manager):
     def get_queryset(self):
         return NoSeriesLineQuerySet(self.model, using=self._db)
 
-    def get_latest_code(self, series_no: str, starting_date: datetime.date, blocked: bool = False) -> Optional['NoSeriesLine']:
+    def get_latest_code(self, series_no: int, starting_date: datetime.date, blocked: bool = False):
         return self.get_queryset().get_latest_code(series_no, starting_date, blocked)
 
 
@@ -35,16 +17,19 @@ class ServiceManager(models.Manager):
         return self.get_queryset().bloked()
 
 
-# class ServiceTypeManager(models.Manager):
-#     def get_queryset(self):
-#         return ServiceTypeQuerySet(self.model, using=self._db)
+class ServicePriceManager(models.Manager):
+    def get_queryset(self):
+        return ServicePriceQuerySet(self.model, using=self._db)
+
+    def fresh_prices(self, service_pk):
+        return self.get_queryset().fresh_prices(service_pk=service_pk)
 
 
 class UnitOfMeasureManager(models.Manager):
     def get_queryset(self):
         return UnitOfMeasureQuerySet(self.model, using=self._db)
 
-    def okei_code(self,okei_code):
+    def okei_code(self, okei_code):
         return self.get_queryset().okei_code(okei_code=okei_code)
 
 
