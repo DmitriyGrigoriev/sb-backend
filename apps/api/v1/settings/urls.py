b4e210c3b395bf8273edd81bb51000fc289bd9c1
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-19 14:26:16 +0300

    Add new endpoints
    feat: Add new endpoints for supporting service prices and series line functionality.

--- apps/api/v1/settings/urls.py
+++ apps/api/v1/settings/urls.py
@@ -4,15 +4,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 # «Billing Setup» («Биллинг Настройка»)
-billing_setup_list = views.BillingSetupModelViewSet.as_view({
-    'get': 'list',
+noseries_setup = views.NoSeriesSetupModelViewSet.as_view({
+    'get': 'retrieve',
+    # 'get': 'list',
     'post': 'create',
 })
-billing_setup_detail = views.BillingSetupModelViewSet.as_view({
+noseries_setup_detail = views.NoSeriesSetupModelViewSet.as_view({
     'get': 'retrieve',
     'put': 'update',
     'putch': 'partial_update',
-    'delete': 'destroy'
 })
 # «VAT Posting Group» («НДС  Учетная Группа»)
 vatposting_group_list = views.VatPostingGroupModelViewSet.as_view({
@@ -48,6 +48,21 @@ service_detail = views.ServiceModelViewSet.as_view({
     'putch': 'partial_update',
     'delete': 'destroy'
 })
+# «Service Price» («Услуга цена»)
+service_price_list = views.ServicePriceModelViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+service_price_detail = views.ServicePriceModelViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'putch': 'partial_update',
+    'delete': 'destroy'
+})
+service_price_by_service_list = views.ServicePriceModelViewSet.as_view({
+    'get': 'service_list',
+    'post': 'service_create',
+})
 # «Unit of Measure» («Единица Измерения»)
 unitofmeasure_list = views.UnitOfMeasureViewSet.as_view({
     'get': 'list',
@@ -75,28 +90,40 @@ noseriesline_list = views.NoSeriesLineViewSet.as_view({
     'get': 'list',
     'post': 'create',
 })
-noseriesline_blocked = views.NoSeriesLineViewSet.as_view({
-    'get': 'blocked_list',
-})
-noseriesline_unblocked = views.NoSeriesLineViewSet.as_view({
-    'get': 'blocked_detail',
-    'put': 'blocked_detail',
-})
 noseriesline_detail = views.NoSeriesLineViewSet.as_view({
     'get': 'retrieve',
     'put': 'update',
     'putch': 'partial_update',
     'delete': 'destroy'
 })
+noseriesline_blocked_list = views.NoSeriesLineViewSet.as_view({
+    'get': 'blocked_list',
+})
+noseriesline_blocked_detail = views.NoSeriesLineViewSet.as_view({
+    'get': 'blocked_retrieve',
+    'put': 'blocked_update',
+    'delete': 'blocked_destroy'
+})
+
+# noseriesline_blocked_detail = views.NoSeriesLineViewSet.as_view({
+#     'get': 'blocked_detail',
+#     'put': 'blocked_detail',
+#     'delete': 'blocked_detail'
+# })
 ### end of # «No. Series Line»
 
 urlpatterns = format_suffix_patterns([
-    # «Billing Setup» («Биллинг Настройка»)
-    path('setup/', billing_setup_list, name='billing_setup_list'),
-    path('setup/<int:pk>/', billing_setup_detail, name='billing_setup_detail'),
+    # «NoSeries Setup» («Биллинг Настройка»)
+    path('setup/', noseries_setup, name='noseries_setup'),
+    path('setup/<int:pk>/', noseries_setup_detail, name='noseries_detail'),
     # «No. Series» («Серия Номеров») код серии номеров
     path('service/', service_list, name='service_list'),
     path('service/<int:pk>/', service_detail, name='service_detail'),
+    # «Service Price» («Услуга, цена»)
+    path('serviceprice/', service_price_list, name='service_price_list'),
+    path('serviceprice/<int:pk>/', service_price_detail, name='service_price_detail'),
+    # «Service Price» («Услуга, цена») фильт по service_id
+    path('serviceprice/<int:pk>/service', service_price_by_service_list, name='service_price_by_service_list'),
     # «Service Type» («Тип Услуги»)
     path('servicetype/', servicetype_list, name='servicetype_list'),
     path('servicetype/<int:pk>/', servicetype_detail, name='servicetype_detail'),
@@ -107,8 +134,9 @@ urlpatterns = format_suffix_patterns([
     path('noseriesline/', noseriesline_list, name='noseriesline_list'),
     path('noseriesline/<int:pk>/', noseriesline_detail, name='noseriesline_detail'),
     # «No. Series Line» («Серия Номеров Строка») заблокированные серии номеров
-    path('noseriesline/blocked-list/', noseriesline_blocked, name='noseriesline_blocked'),
-    path('noseriesline/<int:pk>/blocked-detail/', noseriesline_unblocked, name='noseriesline_unblocked'),
+    path('noseriesline/blocked/', noseriesline_blocked_list, name='noseriesline_blocked_list'),
+    path('noseriesline/blocked/<int:pk>/', noseriesline_blocked_detail, name='noseriesline_blocked_detail'),
+    # path('noseriesline/blocked/<int:pk>/', noseriesline_blocked_detail, name='noseriesline_blocked_detail'),
     ### «Unit of Measure» («Единица Измерения»)
     path('unitofmeasure/', unitofmeasure_list, name='unitofmeasure_list'),
     path('unitofmeasure/<int:pk>/', unitofmeasure_detail, name='unitofmeasure_detail'),
