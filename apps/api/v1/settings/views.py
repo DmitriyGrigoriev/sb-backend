c09e2a02f4b1367ee9fa702b3e9251104a330957
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-19 14:50:37 +0300

    Add some swagger schemes and decorators

--- apps/api/v1/settings/views.py
+++ apps/api/v1/settings/views.py
@@ -1,25 +1,37 @@
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
 from common.viewsets import *
-from rest_framework import generics, viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.decorators import action
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from .serializers import *
 
+
 ### «Unit of Measure» («Единица Измерения»)
 class UnitOfMeasureViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
-    queryset = UnitOfMeasure.objects.all()
-    serializer_class = UnitOfMeasureDetailSerializer
-    filterset_fields = ['code', 'okei_code']
+    queryset = UnitOfMeasure.objects.all().order_by('code')
+    serializer_class = UnitOfMeasureListSerializer
+    # pagination_class = PaginationData
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['code', 'okei_code', 'description']
+    # filter_backends = (DjangoFilterBackend,)
+    # filterset_fields = ['code', 'okei_code']
 
-# class UnitOfMeasureDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = UnitOfMeasure.unit_of_measure.all()
-#     serializer_class = UnitOfMeasureListSerializer
+    def validate_code(self, value):
+        if len(value) > 0 :
+            value = value.lower()
+        return value
 
 
 ### «Service Type» («Тип Услуги»)
 class ServiceTypeModelViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
+    """
+    View: «Тип Услуги»
+    """
+    """
+    Model :model:`settings.servicetype`
+    """
     queryset = ServiceType.objects.all()
     serializer_class = ServiceTypeSerializer
 
@@ -31,74 +43,95 @@ class VatPostingGroupModelViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
 
 
 ### «Service» («Услуга, цена»)
-class BillingSetupModelViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
-    queryset = BillingSetup.billind_setup.get_series_no_setup()
-    serializer_class = BillingSetupSerializer
-
-    serializer_classes = {
-        'create': BillingSetupSerializer,
-        'update': BillingSetupSerializer,
-    }
-
-
-### «Service» («Услуга, цена»)
 class ServiceModelViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
-    queryset = Service.sevice_manager.all()
+    queryset = Service.service_manager.all()
     serializer_class = ServiceListSerializer
 
     serializer_classes = {
         'create': ServiceCreateSerializer,
-        'update': ServiceCreateSerializer,
+        'update': ServiceUpdateSerializer,
+        'retrieve': ServiceUpdateSerializer,
     }
 
+
 ### «Service Price» («Услуга, цена»)
-class ServicePriceList(generics.ListAPIView):
+class ServicePriceModelViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
+    SERVICE_LIST = 'service_list'
+    SERVICE_CREATE = 'service_create'
+
     queryset = ServicePrice.objects.all()
-    serializer_class = ServicePriceListSerializer
+    serializer_classes = {
+        'service_list': ServicePriceSerializer,
+    }
+    serializer_class = ServicePriceSerializer
+
+    @action(detail=False)
+    def service_list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @action(detail=False)
+    def service_create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def get_queryset(self):
+        if self.action == self.SERVICE_LIST:
+            self.queryset = self.get_service_queryset()
+        elif self.action == self.SERVICE_CREATE:
+            self.queryset = self.get_service_queryset()
+
+        return super().get_queryset()
+
+    def get_service_queryset(self):
+        return ServicePrice.service_price.fresh_prices(service_pk=self.kwargs['pk'])
 
 
 @method_decorator(name='list', decorator=swagger_auto_schema(
     operation_description=_("«No. Series» («Серия Номеров») код серий номеров"),
     operation_summary=_("Код серий номеров (список)"),
-    responses={status.HTTP_200_OK: openapi.Response(_('Код серии номеров'), NoSeriesListSerializer)}
+    responses={status.HTTP_200_OK: openapi.Response(_('Код серии номеров'), NoSeriesSerializer)}
 ))
 @method_decorator(name='update', decorator=swagger_auto_schema(
     operation_description=_('«No. Series» («Серия Номеров») редактировать код серий номеров'),
     operation_summary=_('Изменение кода серийного номера по id'),
-    responses={status.HTTP_200_OK: openapi.Response(_('Измененный код серии номеров'), NoSeriesCreateSerializer)}
+    responses={status.HTTP_200_OK: openapi.Response(_('Измененный код серии номеров'), NoSeriesSerializer)}
 ))
 @method_decorator(name='create', decorator=swagger_auto_schema(
     operation_description=_('«No. Series» («Серия Номеров») создать новый код серии номеров'),
     operation_summary=_('Создать новый код серии номеров'),
-    responses={status.HTTP_201_CREATED: openapi.Response(_('Новый код серии номеров'), NoSeriesCreateSerializer)}
+    responses={status.HTTP_201_CREATED: openapi.Response(_('Новый код серии номеров'), NoSeriesSerializer)}
 ))
 @method_decorator(name='retrieve', decorator=swagger_auto_schema(
     operation_description=_('«No. Series» («Серия Номеров») код серии номеров'),
     operation_summary=_('Получить код серии номеров по id:'),
-    responses={status.HTTP_200_OK: openapi.Response(_('Код серии номеров'), NoSeriesListSerializer)}
+    responses={status.HTTP_200_OK: openapi.Response(_('Код серии номеров'), NoSeriesSerializer)}
 ))
 @method_decorator(name='destroy', decorator=swagger_auto_schema(
     operation_description=_('«No. Series» («Серия Номеров») удаление кода серии номеров'),
     operation_summary=_('Удаление кода серии номеров по id:'),
-    responses={status.HTTP_204_NO_CONTENT: openapi.Response(_('Код серии номеров для удаления'), NoSeriesListSerializer)}
+    responses={status.HTTP_204_NO_CONTENT: openapi.Response(_('Код серии номеров для удаления'), NoSeriesSerializer)}
 ))
 class NoSeriesViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
     queryset = NoSeries.no_series.all()
-    serializer_class = NoSeriesListSerializer
+    serializer_class = NoSeriesSerializer
     serializer_classes = {
-        'create': NoSeriesCreateSerializer,
-        'update': NoSeriesCreateSerializer,
+        'create': NoSeriesSerializer,
+        'update': NoSeriesSerializer,
     }
 
 
 @method_decorator(name='list', decorator=swagger_auto_schema(
     operation_description=_('«No. Series Line» («Серия Номеров Строка») актуальные серии номеров blocked=False'),
     operation_summary=_("Актуальные серии номеров (список)"),
-    responses={status.HTTP_200_OK: openapi.Response(_('Список серий номеров'), NoSeriesLineCommonSerializer)}
+    responses={status.HTTP_200_OK: openapi.Response(_('Список актуальных серий номеров'), NoSeriesLineCommonSerializer)}
+))
+@method_decorator(name='blocked_list', decorator=swagger_auto_schema(
+    operation_description=_('«No. Series Line» («Серия Номеров Строка») не актуальные серии номеров blocked=True'),
+    operation_summary=_("Заблокированные Серии Номеров (список)"),
+    responses={status.HTTP_200_OK: openapi.Response(_('Список заблокированных серий номеров'), NoSeriesLineCommonSerializer)}
 ))
 @method_decorator(name='update', decorator=swagger_auto_schema(
     operation_description=_('«No. Series Line» («Серия Номеров Строка») редактировать серийный номер'),
-    operation_summary=_('Изменение серийного номера по его id'),
+    operation_summary=_('Изменение серийного номера по его ID'),
     responses={status.HTTP_200_OK: openapi.Response(_('Измененная серия номеров'), NoSeriesLineCommonSerializer)}
 ))
 @method_decorator(name='create', decorator=swagger_auto_schema(
@@ -113,20 +146,25 @@ class NoSeriesViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
 ))
 @method_decorator(name='destroy', decorator=swagger_auto_schema(
     operation_description=_('«No. Series Line» («Серия Номеров Строка») удаление серии номеров'),
-    operation_summary=_('Удаление серии номеров по id:'),
+    operation_summary=_('Удаление серии номеров по ID:'),
     responses={status.HTTP_204_NO_CONTENT: openapi.Response(_('Серия номеров для удаления'), NoSeriesLineCommonSerializer)}
 ))
+
 
 ### https://stackoverflow.com/questions/52092805/add-swagger-description-in-comments
 class NoSeriesLineViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
     BLOCKED_LIST = 'blocked_list'
-    BLOCKED_DETAIL = 'blocked_detail'
+    BLOCKED_UPDATE = 'blocked_update'
+    BLOCKED_RETRIEVE = 'blocked_retrieve'
+    BLOCKED_DESTROY = 'blocked_destroy'
 
     queryset = NoSeriesLine.series_line.filter(blocked=False).order_by('-starting_date')
     serializer_classes = {
         'create': NoSeriesLineCreateSerializer,
         'update': NoSeriesLineCreateSerializer,
-        'blocked_detail': NoSeriesLineUnblockedDetailSerializer,
+        'blocked_update': NoSeriesLineCreateSerializer,
+        'blocked_retrieve': NoSeriesLineCreateSerializer,
+        'blocked_destroy': NoSeriesLineCreateSerializer,
     }
     serializer_class = NoSeriesLineCommonSerializer
 
@@ -139,26 +177,85 @@ class NoSeriesLineViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
     def blocked_list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
+    @swagger_auto_schema(
+        operation_description=_('«No. Series Line» («Серия Номеров Строка») не актуальный серийный номер'),
+        operation_summary=_('Обновить заблокированный Серийный Номер по его ID:'),
+        responses={status.HTTP_200_OK: openapi.Response(_('Заблокированная серия номеров'), NoSeriesLineBlockedDetailSerializer)}
+    )
+    @action(detail=False)
+    def blocked_update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
 
     @swagger_auto_schema(
         operation_description=_('«No. Series Line» («Серия Номеров Строка») не актуальный серийный номер'),
-        operation_summary=_('Получить не актуальный серийный номер по id:'),
-        responses={status.HTTP_200_OK: openapi.Response(_('Заблокированная серия номеров'), NoSeriesLineUnblockedDetailSerializer)}
+        operation_summary=_('Получить заблокированный Серийный Номер по его ID:'),
+        responses={status.HTTP_200_OK: openapi.Response(_('Заблокированная серия номеров'), NoSeriesLineBlockedDetailSerializer)}
     )
     @action(detail=False)
-    def blocked_detail(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
+    def blocked_retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description=_('«No. Series Line» («Серия Номеров Строка») не актуальный серийный номер'),
+        operation_summary=_('Удалить заблокированный Серийный Номер по его ID:'),
+        responses={status.HTTP_200_OK: openapi.Response(_('Заблокированная серия номеров'), NoSeriesLineBlockedDetailSerializer)}
+    )
+    @action(detail=False)
+    def blocked_destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
 
     def get_queryset(self):
         if self.action == self.BLOCKED_LIST:
             self.queryset = self.get_blocked_queryset()
-        elif self.action == self.BLOCKED_DETAIL:
-            self.queryset = self.get_blocked_detail_queryset()
+        elif self.action == self.BLOCKED_RETRIEVE:
+            self.queryset = self.get_blocked_retrieve_queryset()
+        elif self.action == self.BLOCKED_UPDATE:
+            self.queryset = self.get_blocked_retrieve_queryset()
+        elif self.action == self.BLOCKED_DESTROY:
+            self.queryset = self.get_blocked_retrieve_queryset()
 
         return super().get_queryset()
 
     def get_blocked_queryset(self):
-        return NoSeriesLine.series_line.filter(blocked=True)
+        return NoSeriesLine.series_line.filter(blocked=True).order_by('-starting_date')
 
-    def get_blocked_detail_queryset(self):
+    def get_blocked_retrieve_queryset(self):
         return NoSeriesLine.series_line.filter(pk=self.kwargs.get(self.lookup_field), blocked=True)
+
+
+### Setup «NoSeriesSetup» («Настройка Биллинга»)
+class NoSeriesSetupModelViewSet(AtomicModelViewSet, viewsets.ModelViewSet):
+    # queryset = NoSeriesSetup.objects.none()
+    serializer_class = NoSeriesSetupSerializer
+
+    serializer_classes = {
+        'create': NoSeriesSetupSerializer,
+        'update': NoSeriesSetupSerializer,
+    }
+    def get_queryset(self):
+        queryset = NoSeriesSetup.objects.none()
+        if self.action == 'retrieve':
+            queryset = self.retrieve_queryset()
+        elif self.action == 'update':
+            queryset = self.update_queryset()
+        elif self.action == 'create':
+            queryset = self.create_queryset()
+        return queryset
+
+    def retrieve_queryset(self):
+        count = NoSeriesSetup.objects.count()
+        pk = 0
+        if count != 0:
+            pk = NoSeriesSetup.objects.first().pk
+
+        self.kwargs.update({'pk': pk})
+        queryset = NoSeriesSetup.objects.filter(pk=pk)
+
+        return queryset
+
+    def update_queryset(self):
+        return NoSeriesSetup.objects.all()
+
+    def create_queryset(self):
+        return NoSeriesSetup.objects.all()
