a0243d63d5b38a31bdb4c414506b05de5ceb7075
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-19 15:03:39 +0300

    Add auto-increment setting

--- apps/settings/apps.py
+++ apps/settings/apps.py
@@ -3,5 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class SettingsConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'apps.settings'
     verbose_name = _('Настройки')
