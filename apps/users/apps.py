8ce3b2b66608b6f30cae9236999788e9543a600b
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-20 10:11:52 +0300

    Add default auto field settings

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -3,5 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'apps.users'
     verbose_name = _('Пользователи')
