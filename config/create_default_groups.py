b1a5c890f5992a5ed814a0906f0afc2b51ec3df5
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-22 21:19:16 +0300

    Initial commit

---
+++ config/create_default_groups.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Group
+# import os
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+#
+# import django
+#
+# django.setup()
+# from django.contrib.auth.models import Group
+
+
+def CreateTestGroup():
+    GROUPS = ['administrator', 'superuser', 'guest', 'reviewer', 'encoder']
+    MODELS = ['user']
+
+    for group in GROUPS:
+        new_group, created = Group.objects.get_or_create(name=group)
