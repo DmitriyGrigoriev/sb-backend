933a64f0e16690a7dc0ff443d29632e8568ae33c
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-22 17:01:47 +0300

    Create TemplateAdminModel

--- apps/transportconf/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Terminal
-from common.models import TemplateAdminModel
-
-@admin.register(Terminal)
-class TerminalAdmin(TemplateAdminModel):
-    pass
