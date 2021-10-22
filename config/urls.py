a4c9f744556795b3d9c5531bdcaa4f493643faea
Dmitriy Grigorev <dimag@inbox.ru>
2021-10-22 21:18:44 +0300

    Remove unused views

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import include
-from django.contrib.auth import login
-from django.contrib.auth import logout
 from django.conf.urls.static import static
 from .yasg import urlpatterns as doc_api_urls
 
@@ -11,30 +9,25 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView, TokenVerifyView
 )
 
-from apps.api.v1.token.views import (
-    DecoratedTokenObtainPairView, DecoratedTokenRefreshView
-)
-
-# from config.api import api
-
-# from apps.transportconf.views import TokenObtainPairView
-
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')), # grappelli URLS
     # path('login/', login, {'next_page': '/'}, name='login'),
-    # path('logout/', logout, {'next_page': '/'}, name='logout'),
+    # path('logout/', logout, {'next_page': '/'}, name='logout')
+    # path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls, name='admin'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    #path to djoser end points
-    path('api/auth/', include('djoser.urls')),
+    # path to djoser end points
+    # path('auth/', include('djoser.urls')), ## replace djoser.urls to users.urls
+    # add some filters
+    path('auth/', include('users.urls')),
     ### https://djoser.readthedocs.io/en/latest/authentication_backends.html
     ### Disable Token Based Authentication
     # path('api/auth/', include('djoser.urls.authtoken')),
 
     ### https://djoser.readthedocs.io/en/latest/jwt_endpoints.html
     ### JSON Web Token Authentication
-    path('api/auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls.jwt')),
 
     ### https://django-rest-framework-simplejwt.readthedocs.io
     ### https://github.com/axnsan12/drf-yasg/issues/407
@@ -46,7 +39,8 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-    path('api/v1/', include('apps.api.v1.settings.urls'))
+    path('api/v1/', include('apps.api.v1.settings.urls')),
+    # path('api/v1/', include('apps.api.v1.cms.urls'))
 
 ]
 # Todo Решить позже делать ли документацию api публичной
