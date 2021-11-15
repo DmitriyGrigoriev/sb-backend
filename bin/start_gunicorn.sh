355c2d6fac95919222a31e17065ed2705f635bfd
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-15 11:33:37 +0300

    fix: Update gunicorn scripts

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-source /home/www/code/backend/sb/env/bin/activate
-cd /home/www/code/backend/sb
-exec gunicorn -c "/home/www/code/backend/sb/config/gunicorn_config.py" config.wsgi
+NAME="smart-billing"                              # Name of the application
+DJANGODIR=/home/www/code/backend/sb               # Django project directory
+
+echo "Starting $NAME"
+
+# Activate the virtual environment
+cd $DJANGODIR
+
+source ../env/bin/activate
+exec gunicorn -c "../config/gunicorn_config.py" config.wsgi
+
+# source /home/www/code/backend/sb/env/bin/activate
+#cd /home/www/code/backend/sb
+#exec gunicorn -c "/home/www/code/backend/sb/config/gunicorn_config.py" config.wsgi
